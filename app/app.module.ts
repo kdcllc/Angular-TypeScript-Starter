37875da40b7fb2fba3b285501a85ed4976dff615
KDCLLC <info@kingdavidconsulting.com>
2016-02-17 15:32:27 -0500

    Add initial app code to the repository

---
+++ app/app.module.ts
@@ -0,0 +1,29 @@
+/// <reference path="../tools/typings/tsd/tsd.d.ts" />
+
+import { config } from './app.config';
+import { routerConfig } from './app.route';
+import { runBlock } from './app.run';
+import { MainController } from './main/main.controller';
+import { GithubContributor } from '../app/components/githubContributor/githubContributor.service';
+import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { acmeNavbar } from '../app/components/navbar/navbar.directive';
+import { acmeMalarkey } from '../app/components/malarkey/malarkey.directive';
+
+declare var malarkey: any;
+declare var moment: moment.MomentStatic;
+
+module test {
+  'use strict';
+
+  angular.module('test', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr'])
+    .constant('malarkey', malarkey)
+    .constant('moment', moment)
+    .config(config)
+    .config(routerConfig)
+    .run(runBlock)
+    .service('githubContributor', GithubContributor)
+    .service('webDevTec', WebDevTecService)
+    .controller('MainController', MainController)
+    .directive('acmeNavbar', acmeNavbar)
+    .directive('acmeMalarkey', acmeMalarkey);
+}
